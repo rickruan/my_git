ef04a70da1d7ef9b705f80feb399c59d80c25811
rickruan <ruanchenghua@hotmail.com>
2018-12-19 09:18:04 +0800

    web app

---
+++ CEP_python_web/www/uuid_test.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__='Rick Ruan'
+
+import uuid
+
+name = 'test_name'
+
+namespace = uuid.NAMESPACE_URL
+
+print(uuid.uuid1())
+print(uuid.uuid3(namespace,name))
+print(uuid.uuid4())
+print(uuid.uuid5(namespace,name))
